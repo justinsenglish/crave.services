dce40878e607bfc008404b8f493909b9ec40b538
Justin English <hoshafe@gmail.com>
2024-12-14 12:24:30 -0700

    Initial commit

---
+++ test/crave.services.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CraveServices from '../lib/crave.services-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/crave.services-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CraveServices.CraveServicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
